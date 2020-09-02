7408c5017e4624318b1128dd8707133e245411a4
dynnammo <baptiste.thivend@insa-lyon.fr>
2020-09-02 15:58:39 +0200

    Correct tests

--- lib/decidim/calendar/test/factories.rb
+++ lib/decidim/calendar/test/factories.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :external_event, class: Decidim::Calendar::ExternalEvent do
+  factory :external_event, class: "Decidim::Calendar::ExternalEvent" do
     title { generate_localized_title }
     start_at { Date.current }
     end_at { Date.current + 1.day }
--- spec/system/admin/help_sections_spec.rb
+++ spec/system/admin/help_sections_spec.rb
@@ -2,18 +2,19 @@
 
 require "spec_helper"
 
-describe "manage calendar's module help section" do
+describe "manage help section" do
   let(:organization) { create(:organization) }
   let(:user) { create(:user, :admin, :confirmed, organization: organization) }
 
   before do
-    Capybara.ignore_hidden_elements = false
     switch_to_host(organization.host)
     login_as user, scope: :user
     visit decidim_admin.help_sections_path
   end
-  
-  it "displays help sections for calendar" do
+
+  it "displays help section for calendar" do
+    Capybara.ignore_hidden_elements = false
     expect(page).to have_content("Calendars")
+    Capybara.ignore_hidden_elements = true
   end
 end
